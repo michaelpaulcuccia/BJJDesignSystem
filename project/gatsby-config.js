374f6cdc49327ed45b6a83499f1201a6753ba134
Michael Cuccia <michael.cuccia@egplusww.com>
2021-08-26 11:30:38 -0400

    updated config

--- project/gatsby-config.js
+++ project/gatsby-config.js
@@ -6,9 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
